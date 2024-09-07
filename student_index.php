1e85eaa2ba2bd8dc94852bf58255485915f92263
Pinocus <cupstone@live.com>
2024-09-07 20:20:41 +0000

    Add files via upload

---
+++ student_index.php
@@ -0,0 +1,33 @@
+
+
+
+
+
+<div>
+<marquee> Welcome to Kasoa Stake YSA Gathering Place </marquee>
+</div>
+
+<div>
+<?php
+ include 'student_menu.php';
+?>
+</div>
+
+
+<h3></h3>
+<p></p>
+
+
+
+
+
+
+
+
+
+
+<div>
+<?php
+ include '<includes/footer.php';
+?>
+</div>
