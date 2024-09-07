87bc735912952c2825fa3d6fb46acfe84e9143d2
Pinocus <cupstone@live.com>
2024-09-07 20:21:37 +0000

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div>
+<marquee> Kasoa Stake YSA Gathering Place </marquee>
+</div>
+
+
+
+
+<div>
+<?php
+echo "<p>Copyright &copy;" . date("Y") . " kasoastake.org</p>";
+?>
+</div>
+
+</body>
+</html>
